bb2d65571eec852f8b11d2d867c0b92d7e5e60df
ARIJIT SENGUPTA <sengupta.arijit84@gmail.com>
2022-05-28 19:15:16 +0530

    create solidity and migrate

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,6 @@
 const Migrations = artifacts.require("Migrations");
+const Todolist = artifacts.require("./Todolist.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Todolist);
 };
