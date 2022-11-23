0fe820793ce87d5f67c70817a30149407fb66e41
ARIJIT SENGUPTA <sengupta.arijit84@gmail.com>
2022-11-23 21:05:07 +0530

    implement todolist functions

--- contracts/Todolist.sol
+++ contracts/Todolist.sol
@@ -12,13 +12,32 @@ contract Todolist {
     
     mapping(uint => Task) public tasks;
 
+    event TaskCreated(
+        uint id,
+        string content,
+        bool complete
+    );
+
+    event TaskCompleted(
+        uint id,
+        bool complete
+    );
+
     //constructor
     constructor() public{
-        createTask("Constructor Called");
+        createTask("List Down Your First Task");
     }
 
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount] = Task(taskCount,_content,false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+    
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.complete = !_task.complete;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id,  _task.complete);
     }
 }
--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ App = {
     load: async () => {
         console.log("app loading");
         await App.loadWeb3();
-        //await App.loadAccount();
-       // await App.loadContract();
+        await App.loadAccount();
+        await App.loadContract();
+        await App.render();
 
     },
     loadWeb3: async () => {
@@ -37,6 +38,82 @@ App = {
         else {
             console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
         }
+    },
+    loadAccount: async () => {
+        if (typeof window.ethereum !== 'undefined') {
+            const res = await window.ethereum.request({ method: "eth_requestAccounts" });
+            App.account = res[0];
+            console.log(App.account);
+        } else {
+            window.alert("Please connect to Metamask.")
+        }
+    },
+    loadContract: async () => {
+        const todolist = await $.getJSON('Todolist.json');
+        App.contracts.Todolist = TruffleContract(todolist);
+        App.contracts.Todolist.setProvider(App.web3Provider);
+        App.todolist = await App.contracts.Todolist.deployed();
+        console.log(todolist);
+    },
+    setLoading: (value) => {
+        App.loading = value;
+        const loader = $("#loader");
+        const content = $("#content");
+        if (value) {
+            loader.show();
+            content.hide();
+        } else {
+            loader.hide();
+            content.show();
+        }
+    },
+    render: async () => {
+        if (App.loading) {
+            return;
+        }
+
+        App.setLoading(true);
+        $("#account").html(App.account);
+        await App.renderTasks();
+        App.setLoading(false);
+    },
+    renderTasks: async () => {
+        const taskCount = await App.todolist.taskCount();
+        const $taskTemplate = $(".taskTemplate");
+        for (var i = 1; i <= taskCount; i++) {
+            const task = await App.todolist.tasks(i);
+            const taskId = task[0].toNumber();
+            const taskContent = task[1];
+            const taskCompleted = task[2];
+
+            const $newTaskTemplate = $taskTemplate.clone();
+            $newTaskTemplate.find(".content").html(taskContent);
+            $newTaskTemplate.find("input")
+                .prop("name", taskId)
+                .prop("checked", taskCompleted)
+                .on("click", App.toggleCompleted);
+
+            if (taskCompleted) {
+                $("#completedTaskList").append($newTaskTemplate);
+            } else {
+                $("#taskList").append($newTaskTemplate);
+            }
+
+            $newTaskTemplate.show();
+        }
+    },
+    createTask: async () => {
+        App.setLoading(true);
+        const content = document.getElementById("newTask").value;
+        console.log(content)
+        await App.todolist.createTask(content , { "from" : App.account });
+        window.location.reload();
+    },
+    toggleCompleted: async (e) => {
+        App.setLoading(true);
+        const taskId = e.target.name;
+        await App.todolist.toggleCompleted(taskId , {"from": App.account})
+        window.location.reload();
     }
 }
 
